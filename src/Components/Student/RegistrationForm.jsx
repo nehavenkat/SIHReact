8be088154cb1cc9dde94a0791c877f9e34b16434
nehavenkat <neha.venkatsetty@gmail.com>
2020-07-03 14:47:15 +0200

    gender selection

--- src/Components/Student/RegistrationForm.jsx
+++ src/Components/Student/RegistrationForm.jsx
@@ -1,5 +1,5 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/material.css'
 import TextField from "@material-ui/core/TextField";
@@ -11,96 +11,96 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
-import  "../Student/RegistrationForm.css"
+import "../Student/RegistrationForm.css"
 
 
 export default class RegistrationForm extends Component {
     render() {
         return (
             <div>
-                 <div className="container">
-                <div className="studentRegistrationBox">
-                    <form noValidate autoComplete="off">
-                        <div className={"row mt-4"}>
-                            <div className={"col-md-6"}>
-                                <TextField id="outlined-basic" label="First Name" variant="outlined"/>
+                <div className="container">
+                    <div className="studentRegistrationBox">
+                        <form noValidate autoComplete="off">
+                            <div className={"row mt-4"}>
+                                <div className={"col-md-6"}>
+                                    <TextField id="outlined-basic" label="First Name" variant="outlined" />
+                                </div>
+                                <div className={"col-md-6"}>
+                                    <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+                                </div>
                             </div>
-                            <div className={"col-md-6"}>
-                                <TextField id="outlined-basic" label="Last Name" variant="outlined"/>
-                            </div>
-                        </div>
-                        <div className={"row mt-3"}>
-                            <div className={"col-md-6"}>
-                                <TextField variant="outlined"
-                                           id="Birthday"
-                                           label="Date of birth"
-                                           type="date"
-                                           InputLabelProps={{
-                                               shrink: true,
-                                           }}
-                                />
-                            </div>
-                            <div className={"col-md-6"}>
-                                <FormControl variant="outlined">
-                                    <InputLabel id="demo-simple-select-outlined-label">Gender</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-outlined-label"
-                                        id="demo-simple-select-outlined"
-                                        label="Gender"
-                                    >
-                                        <MenuItem>Female</MenuItem>
-                                        <MenuItem>Male</MenuItem>
-                                        <MenuItem>Other</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </div>
-                        </div>
-
-                        <div className={"row mt-4"}>
-                            <div className="col-md-6">
-                                <FormControl variant="outlined">
-                                    <DropDownCountriesForm/>
-                                </FormControl>
-                            </div>
-                            <div className="col-md-6">
-                                <FormControl variant="outlined">
-                                    <PhoneInput
-                                        country={'dk'}
-                                        inputProps={{
-                                            label: 'Phone',
-                                            required: true,
+                            <div className={"row mt-3"}>
+                                <div className={"col-md-6"}>
+                                    <TextField variant="outlined"
+                                        id="Birthday"
+                                        label="Date of birth"
+                                        type="date"
+                                        InputLabelProps={{
+                                            shrink: true,
                                         }}
                                     />
-                                </FormControl>
+                                </div>
+                                <div className={"col-md-6"}>
+                                    <FormControl variant="outlined" >
+                                        <InputLabel id="outlined-basic">Gender</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-outlined-label"
+                                            id="outlined-basic"
+                                            label="Gender">
+                                            <MenuItem value="">
+                                            </MenuItem>
+                                            <MenuItem value={10}>Male</MenuItem>
+                                            <MenuItem value={20}>Female</MenuItem>
+                                            <MenuItem value={30}>Other</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </div>
                             </div>
-                        </div>
-                        <div className="row mt-4">
-                            <div className="col-md-6">
-                                <FormControl variant="outlined">
-                                    <TextField id="outlined-basic" label="E-mail" variant="outlined"/>
-                                </FormControl>
+
+                            <div className={"row mt-4"}>
+                                <div className="col-md-6">
+                                    <FormControl variant="outlined-basic">
+                                        <DropDownCountriesForm />
+                                    </FormControl>
+                                </div>
+                                <div className="col-md-6">
+                                    <FormControl variant="outlined-basic">
+                                        <PhoneInput
+                                            country={'dk'}
+                                            inputProps={{
+                                                label: 'Phone',
+                                                required: true,
+                                            }}
+                                        />
+                                    </FormControl>
+                                </div>
                             </div>
-                            <div className="col-md-6 text-left">
-                                <FormControlLabel
-                                    control={<Checkbox name="checked" className="ml-4 mt-1" color="primary"/>}
-                                    label="Receive Newsletter"
-                                />
+                            <div className="row mt-4">
+                                <div className="col-md-6">
+                                    <FormControl variant="outlined">
+                                        <TextField id="outlined-basic" label="E-mail" variant="outlined" />
+                                    </FormControl>
+                                </div>
+                                <div className="col-md-6 text-left">
+                                    <FormControlLabel
+                                        control={<Checkbox name="checked" className="ml-4 mt-1" color="primary" />}
+                                        label="Receive Newsletter"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div className="row mt-4 mb-4">
-                            <div className="col text-left ml-4">
-                                <Button variant="contained" color="secondary">
-                                    Submit
+                            <div className="row mt-4 mb-4">
+                                <div className="col text-left ml-4">
+                                    <Button variant="contained" color="secondary">
+                                        Submit
                                 </Button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
             </div>
 
 
         );
     }
 }
-                
